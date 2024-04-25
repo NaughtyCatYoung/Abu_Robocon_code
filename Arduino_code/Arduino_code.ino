4bc1790d7b695764463d059550a7d5bf95e7300f
NaughtyCatYoung <witchaya1507@gmail.com>
2024-04-25 14:23:54 +0700

    make code work

--- Arduino_code/Arduino_code.ino
+++ Arduino_code/Arduino_code.ino
@@ -176,6 +176,12 @@ PIDController pos1_pid,pos2_pid;
 int encoder_pos1=0,encoder_pos2=0;
 int encoder_target1=0,encoder_target2=0;
 
+MCT_HB_40A_H_Bridge wheel1(4,41,42);
+MCT_HB_40A_H_Bridge wheel2(5,43,44);
+
+Cytron_20a_motor_driver rotate1(6,45);
+Cytron_20a_motor_driver rotate2(7,46);
+
 void encoder1(){if(digitalRead(26)==HIGH)encoder_pos1++;else encoder_pos1--;}
 void encoder2(){if(digitalRead(27)==HIGH)encoder_pos2++;else encoder_pos2--;}
 
@@ -228,5 +234,117 @@ void loop()
         }
         Serial.println("==================================");
     }
+    if(sz!=0)
+    {
+        Serial.println("It finally works");
+    if(cmds[0]=="DC")
+    {
+        Serial.println("DC motor control");
+        if(sz!=4)
+        {
+            Serial.println("Command error");
+        }
+        else
+        {
+            if(cmds[1]=="1")
+            {
+                if(cmds[2]=="Forward")
+                wheel1.set_speed(string_to_int(cmds[3]));
+                else wheel1.set_speed(-string_to_int(cmds[3]));
+            }
+            else if(cmds[1]=="2")
+            {
+                if(cmds[2]=="Forward")
+                wheel2.set_speed(string_to_int(cmds[3]));
+                else wheel2.set_speed(-string_to_int(cmds[3]));
+            }
+            ;
+        }
+    }
+    else if(cmds[0]=="DC_encoder")
+    {
+        Serial.println("DC_encoder");
+        if(sz!=3)
+        {
+            Serial.println("Command error");
+        }
+        else
+        {
+            if(cmds[1]=="1")
+            {
+                encoder_target1=string_to_int(cmds[2]);
+                pos1_pid.setpoint(-encoder_target1);
+            }
+            else if(cmds[1]=="2")
+            {
+                encoder_target2=string_to_int(cmds[2]);
+                pos2_pid.setpoint(-encoder_target2);
+            }
+            ;
+        }
+    }
+
+    else if(cmds[0]=="Stepper")
+    {
+        // Serial.println("Stepper_motor");
+        // if(sz!=4)
+        // {
+        //     Serial.println("Command error");
+        // }
+        // else
+        // {
+        //     if(cmds[1]=="1")
+        //     {
+        //         if(cmds[3]=="Forward")
+        //         Stepper1.step(string_to_int(cmds[2]),true);
+        //         else 
+        //         Stepper1.step(string_to_int(cmds[2]),false);
+
+        //     }
+        //     else if(cmds[1]=="2")
+        //     {
+        //         if(cmds[3]=="Forward")
+        //         Stepper2.step(string_to_int(cmds[2]),true);
+        //         else 
+        //         Stepper2.step(string_to_int(cmds[2]),false);
+        //     }
+        //     else if(cmds[1]=="3")
+        //     {
+        //         if(cmds[3]=="Forward")
+        //         Stepper3.step(string_to_int(cmds[2]),true);
+        //         else 
+        //         Stepper3.step(string_to_int(cmds[2]),false);
+        //     }
+        //     ;
+        // }
+    }
+    else if(cmds[0]=="Servo")
+    {  
+        // Serial.println("Servo_control");
+        // if(sz!=3)
+        // {
+        //     Serial.println("Command error");
+        // }
+        // else
+        // {
+        //     ;
+        // }
+    }
+    }
+    int motor_encoder1_power=pos1_pid.compute(encoder_pos1);
+    int motor_encoder2_power=-pos2_pid.compute(encoder_pos2);
+
+    // rotate1.set_speed(motor_encoder1_power);
+    // rotate2.set_speed(motor_encoder2_power);
+
+    Serial.print(wheel1.speed_now);Serial.print("\t");
+    Serial.print(wheel2.speed_now);Serial.print("\t");
+    Serial.print(rotate1.speed_now);Serial.print("\t");
+    Serial.print(rotate2.speed_now);Serial.print("\t");
+    Serial.print(encoder_pos1);Serial.print("\t");
+    Serial.print(encoder_pos2);Serial.print("\t");
+    Serial.print(encoder_target1);Serial.print("\t");
+    Serial.print(encoder_target2);Serial.print("\t");
+    
     return;
 }
